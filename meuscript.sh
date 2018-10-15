7ca7abe657cd01b6b47548260f787a071fe64a21
Douglas Cananéa <cananeadouglas@gmail.com>
2018-10-15 11:52:12 -0300

    atualizado

--- meuscript.sh
+++ meuscript.sh
@@ -3,5 +3,4 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-whoami
-echo  " o sistem está atualizado. Tenha um Bom Trabalho"
+echo "$USER o sistema está atualizado. Tenha um bom trabalho"
